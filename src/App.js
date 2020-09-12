b4bbcbb258b235d97d56f410528fafe61501968b
Holly Keogh <hollyrkeogh@gmail.com>
2020-09-12 11:59:56 +0800

    add css styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
-import TodoItem from "./components/TodoItem";
-import TodoForm from "./components/TodoForm";
+import TodoItem from "./components/TodoItem/TodoItem";
+import TodoForm from "./components/TodoForm/TodoForm";
+import "./App.css";
+
 
 function App() {
   //variables 
@@ -19,12 +21,14 @@ function App() {
 
   //template
   return (
-  <div>
-    <h1>My To Do list</h1>
-    {todos.map((todo, index) => (
-      <TodoItem todo={todo} key={index} />
-    ))}
-    <TodoForm addTodo={addTodo} />
+  <div className="app">
+    <div className="todo-list">
+      <h1>My To Do list</h1>
+        {todos.map((todo, index) => (
+        <TodoItem todo={todo} key={index} />
+      ))}
+      <TodoForm addTodo={addTodo} />
+    </div>
   </div>
   )
 }
